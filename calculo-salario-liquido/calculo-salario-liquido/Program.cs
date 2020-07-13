73ef74bd3664f2613a436d3886e94ac3d59d842b
Gabriela Menezes <gabimenezess10@gmail.com>
2020-07-13 11:53:38 -0300

    inicio

---
+++ calculo-salario-liquido/calculo-salario-liquido/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculo_salario_liquido
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
