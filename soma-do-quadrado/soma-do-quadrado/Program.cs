efca2e7d1e9d30734c0ed815235acdabf07cf679
Gabriela Menezes <gabimenezess10@gmail.com>
2020-07-13 11:13:21 -0300

    done

---
+++ soma-do-quadrado/soma-do-quadrado/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace soma_do_quadrado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 0;
+            int soma = 0;
+
+            Console.Write("Digite um número: ");
+            a = int.Parse(Console.ReadLine());
+            
+            Console.Write("Digite outro número: ");
+            b = int.Parse(Console.ReadLine());
+
+            soma = ((a * a) + (b * b));
+            Console.WriteLine("Os valores digitados foram: " + a + " e " + b);
+            Console.WriteLine("A soma do quadrado de cada valor lido é " + soma);
+
+
+            Console.ReadKey();
+        }
+    }
+}
