c90cf4d6d3f155b29c926c450bd036a16bfdd051
Gabriela Menezes <gabimenezess10@gmail.com>
2020-07-13 11:32:03 -0300

    done

---
+++ conversor-de-velocidade/conversor-de-velocidade/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conversor_de_velocidade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double km = 0;
+            double ms = 0;
+
+            Console.WriteLine("CONVERSOR DE VELOCIDADE");
+            Console.Write("Digite sua velocidade em Km/h: ");
+            km = double.Parse(Console.ReadLine());
+            ms = km / 3.6;
+
+            Console.WriteLine("Sua velocidade: " +km + "km/h");
+            Console.WriteLine("Conversão: " +ms + " m/s");
+            Console.ReadKey();
+        }
+    }
+}
