4c0617525839ab5d0d7aca1177c105fe426f6df2
Gabriela Menezes <gabimenezess10@gmail.com>
2020-07-13 11:25:11 -0300

    done

---
+++ troca-ordem/troca-ordem/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace troca_ordem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numA = 0;
+            int numB = 0;
+
+            Console.Write("Digite um número: ");
+            numA = int.Parse(Console.ReadLine());
+            Console.Write("Digite outro número: ");
+            numB = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Os números digitados foram: " + numB + " e " + numA);
+
+            Console.ReadKey();
+        }
+    }
+}
