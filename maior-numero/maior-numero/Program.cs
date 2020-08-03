443375bf36f31936876e61c90fe0d0c7d74528f5
Gabriela Menezes <gabimenezess10@gmail.com>
2020-08-03 13:43:07 -0300

    Retorno de maior número done

---
+++ maior-numero/maior-numero/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maior_numero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A = 0;
+            double B = 0;
+
+            Console.Write("Digite o valor de A: ");
+            A = double.Parse(Console.ReadLine());
+            Console.Write("Digite o valor de B: ");
+            B = double.Parse(Console.ReadLine());
+
+            if(A > B)
+            {
+                Console.Write("O maior valor digitado foi " + A);
+            } else if (B > A)
+            {
+                Console.Write("O maior valor digitado foi " + B);
+            } else
+            {
+                Console.Write("Os valores são iguais");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
