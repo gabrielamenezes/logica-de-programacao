57c7cec35ea66a10ad97b65c108cc00426e5d499
Gabriela Menezes <gabimenezess10@gmail.com>
2020-08-03 13:51:09 -0300

    f1-f2-f3 done

---
+++ f1-f2-f3/f1-f2-f3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace f1_f2_f3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero = 0;
+
+            Console.Write("Digite um número: ");
+            numero = double.Parse(Console.ReadLine());
+
+            if (numero <= 10)
+            {
+                Console.Write("F1");
+            } else if (numero > 10 && numero <= 100)
+            {
+                Console.Write("F2");
+            } else if (numero > 100)
+            {
+                Console.Write("F3");
+            } 
+
+            Console.ReadKey();
+        }
+    }
+}
